687f678b3fbc017667a3ab8d8d1c1188c7b7e835
Joyce Santos <119745348+Joyce-Firmino@users.noreply.github.com>
2023-04-26 08:38:22 -0300

    Add questão2

---
+++ CBBAtividadePratica02.java
@@ -0,0 +1,12 @@
+package Colaborativa;
+
+public class CBBAtividadePratica02 {
+    public static void main(String[] args) {
+        int[] meuArray= new int[100];
+        for(int i=0; i<=100; i++){
+            int auxiliar= i+1;
+            meuArray[i]= auxiliar;
+            System.out.println(meuArray[i]);
+        }
+    }
+}
